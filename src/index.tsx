4810d2c22a4f8d8edd3a1ee5333c6ce120700588
Joseph Mukorivo <josemukorivo@gmail.com>
2024-01-15 21:17:35 +0300

    feat: allow users to pass in additional props and export the type

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,48 @@
-import { ReactNode } from "react";
-import "./global.css";
-import { cn } from "./lib/utils";
+import { HTMLAttributes, ReactNode } from 'react';
+import './global.css';
+import { cn } from './lib/utils';
+
+export type MarqueeProps = HTMLAttributes<HTMLDivElement> & {
+  children: ReactNode;
+  direction?: 'left' | 'up';
+  pauseOnHover?: boolean;
+  reverse?: boolean;
+  fade?: boolean;
+  innerClassName?: string;
+  numberOfCopies?: number;
+};
 
 export function Marquee({
   children,
-  direction = "left",
+  direction = 'left',
   pauseOnHover = false,
   reverse = false,
   fade = false,
   className,
   innerClassName,
   numberOfCopies = 2,
-}: {
-  children: ReactNode;
-  direction?: "left" | "up";
-  pauseOnHover?: boolean;
-  reverse?: boolean;
-  fade?: boolean;
-  className?: string;
-  innerClassName?: string;
-  numberOfCopies?: number;
-}) {
+  ...rest
+}: MarqueeProps) {
   return (
     <div
       className={cn(
-        "group flex gap-[1rem] overflow-hidden",
-        direction === "left" ? "flex-row" : "flex-col",
+        'group flex gap-[1rem] overflow-hidden',
+        direction === 'left' ? 'flex-row' : 'flex-col',
         className
       )}
       style={{
         maskImage: fade
           ? `linear-gradient(${
-              direction === "left" ? "to right" : "to bottom"
+              direction === 'left' ? 'to right' : 'to bottom'
             }, transparent 0%, rgba(0, 0, 0, 1.0) 10%, rgba(0, 0, 0, 1.0) 90%, transparent 100%)`
           : undefined,
         WebkitMaskImage: fade
           ? `linear-gradient(${
-              direction === "left" ? "to right" : "to bottom"
+              direction === 'left' ? 'to right' : 'to bottom'
             }, transparent 0%, rgba(0, 0, 0, 1.0) 10%, rgba(0, 0, 0, 1.0) 90%, transparent 100%)`
           : undefined,
       }}
+      {...rest}
     >
       {Array(numberOfCopies)
         .fill(0)
@@ -47,12 +50,12 @@ export function Marquee({
           <div
             key={i}
             className={cn(
-              "flex justify-around gap-[1rem] [--gap:1rem] shrink-0",
-              direction === "left"
-                ? "animate-marquee-left flex-row"
-                : "animate-marquee-up flex-col",
-              pauseOnHover && "group-hover:[animation-play-state:paused]",
-              reverse && "direction-reverse",
+              'flex justify-around gap-[1rem] [--gap:1rem] shrink-0',
+              direction === 'left'
+                ? 'animate-marquee-left flex-row'
+                : 'animate-marquee-up flex-col',
+              pauseOnHover && 'group-hover:[animation-play-state:paused]',
+              reverse && 'direction-reverse',
               innerClassName
             )}
           >
