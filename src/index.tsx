eb33f11fb0eecd99e8e0ec6da223b558cb7e2efa
Joseph Mukorivo <josemukorivo@gmail.com>
2024-01-16 09:53:32 +0300

    Update index.tsx
    Remove single quotes

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import { HTMLAttributes, ReactNode } from 'react';
-import './global.css';
-import { cn } from './lib/utils';
+import { HTMLAttributes, ReactNode } from "react";
+import "./global.css";
+import { cn } from "./lib/utils";
 
 export type MarqueeProps = HTMLAttributes<HTMLDivElement> & {
   children: ReactNode;
-  direction?: 'left' | 'up';
+  direction?: "left" | "up";
   pauseOnHover?: boolean;
   reverse?: boolean;
   fade?: boolean;
@@ -14,7 +14,7 @@ export type MarqueeProps = HTMLAttributes<HTMLDivElement> & {
 
 export function Marquee({
   children,
-  direction = 'left',
+  direction = "left",
   pauseOnHover = false,
   reverse = false,
   fade = false,
@@ -26,19 +26,19 @@ export function Marquee({
   return (
     <div
       className={cn(
-        'group flex gap-[1rem] overflow-hidden',
-        direction === 'left' ? 'flex-row' : 'flex-col',
+        "group flex gap-[1rem] overflow-hidden",
+        direction === "left" ? "flex-row" : "flex-col",
         className
       )}
       style={{
         maskImage: fade
           ? `linear-gradient(${
-              direction === 'left' ? 'to right' : 'to bottom'
+              direction === "left" ? "to right" : "to bottom"
             }, transparent 0%, rgba(0, 0, 0, 1.0) 10%, rgba(0, 0, 0, 1.0) 90%, transparent 100%)`
           : undefined,
         WebkitMaskImage: fade
           ? `linear-gradient(${
-              direction === 'left' ? 'to right' : 'to bottom'
+              direction === "left" ? "to right" : "to bottom"
             }, transparent 0%, rgba(0, 0, 0, 1.0) 10%, rgba(0, 0, 0, 1.0) 90%, transparent 100%)`
           : undefined,
       }}
@@ -50,12 +50,12 @@ export function Marquee({
           <div
             key={i}
             className={cn(
-              'flex justify-around gap-[1rem] [--gap:1rem] shrink-0',
-              direction === 'left'
-                ? 'animate-marquee-left flex-row'
-                : 'animate-marquee-up flex-col',
-              pauseOnHover && 'group-hover:[animation-play-state:paused]',
-              reverse && 'direction-reverse',
+              "flex justify-around gap-[1rem] [--gap:1rem] shrink-0",
+              direction === "left"
+                ? "animate-marquee-left flex-row"
+                : "animate-marquee-up flex-col",
+              pauseOnHover && "group-hover:[animation-play-state:paused]",
+              reverse && "direction-reverse",
               innerClassName
             )}
           >
